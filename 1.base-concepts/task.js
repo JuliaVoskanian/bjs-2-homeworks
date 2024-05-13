4bbc7c24ba01081aff226c1c164f6cfb2b67f3dd
Julia <juliapogosyan@gmail.com>
2024-05-13 21:27:46 +0300

    Done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+"use strict" 
+ 
+function solveEquation(a, b, c) { 
+  let discriminant = b**2 - 4*a*c; 
+  if (discriminant < 0) { 
+      return []; 
+  } else if (discriminant === 0) { 
+      let x = -b / (2*a); 
+      return [x]; 
+  } else { 
+      let y = (-b + Math.sqrt(discriminant)) / (2*a); 
+      let z = (-b - Math.sqrt(discriminant)) / (2*a); 
+      return [y, z]; 
+  } 
+} 
+let roots = solveEquation(1, 7, 2); 
+console.log(roots);
